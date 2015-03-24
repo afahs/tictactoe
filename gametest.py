9d96d2ad3594de85333c955038583165a2cdd3f4
afahs <adam.fahs@gmail.com>
2015-03-23 21:37:30 -0400

    Made it a two player game

--- gametest.py
+++ gametest.py
@@ -6,9 +6,9 @@ print("Welcome to TicTacToe!")
 # Get char
 char = input("Would you like to be \'X\' or \'O\'?")
 if char == 'X':
-    compchar='Y'
+    player2char='Y'
 if char=='Y':
-    compchar='X'
+    player2char='X'
 # TODO: Check if char is 'X' or 'O'
 
 # Check whether or not to display instructions
@@ -22,12 +22,12 @@ initBoard()
 # Start the game
 while True:
     drawBoard()
-    n = int(input("Where would you like to move?"))
-    if n!=11 or 12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 21 or 22 or 23 or 24 or 25 or 26 or 27 or 28 or 29 or 31 or 32 or 33 or 34 or 35 or 36 or 37 or 38 or 39:
-        print("invalid move!")
+    n = int(input("Where would player one like to move?"))
     if not move(n, char):
         print("Invalid move!")
     if isWinner(board,char):
         print("You win!")
+    n2 = int(input("Where would player two like to move?"))
     input()
+    
     
