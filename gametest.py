149258381995418ee372f66960f12779977d3b5d
afahs <adam.fahs@gmail.com>
2015-03-24 22:16:00 -0400

    Fixing it up

--- gametest.py
+++ gametest.py
@@ -2,13 +2,12 @@ from boardfunc import *
 
 # Print welcome message
 print("Welcome to TicTacToe!")
-char1='X'
-char2='Y'
+
 # Get char
-char = input("Would you like to be \'X\' or \'O\'?")
+char = input("Would player two like to be \'X\' or \'O\'?")
 if char == 'X':
-    player2char='Y'
-if char=='Y':
+    player2char='O'
+if char=='O':
     player2char='X'
 # TODO: Check if char is 'X' or 'O'
 
@@ -30,14 +29,14 @@ def changechars():
 # Start the game
 while True:
     drawBoard()
-    n = int(input("Where would player one like to move?"))
     changechars()
+    n = int(input("Where would " + char + "like to move?"))
     if not move(n, char):
         print("Invalid move!")
     if isWinnerX(board,char):
         print("X wins!")
     if isWinnerO(board,char):
         print("O wins!")
-    input()
+    
     
     
